dc56e0dd5b2d1b0110df30d76ab8aa2c80639043
ElisaMavilia <elisamavilia1@gmail.com>
2024-04-04 19:05:18 +0200

    added the sendMessage method to input (in send-message) + luxon date/time format

--- js/script.js
+++ js/script.js
@@ -9,20 +9,31 @@ createApp({
             contacts: contacts,
             time: "12:30",
             lastMessage: "Ultimo messaggio",
-            activeUserId: 1
+            activeUserId: 1,
+            userMessage: ''
         }
     },
     methods:{
         setActiveUser(id){
             this.activeUserId = id;
+        },
+
+        sendMessage(e){
+            const newMessage = {
+                date: dt.now().setLocale('it').toFormat('dd/MM/yyyy hh:mm:ss'),
+                message: this.userMessage,
+                status: 'sent'
+            }
+            this.activeContact.messages.push(newMessage);
+            this.userMessage = "";
         }
         
-       
     },
     computed: {
         activeContact(id){
             return this.contacts.find((el)=> el.id === this.activeUserId);
         }
+       
     },
 
     mounted(){
